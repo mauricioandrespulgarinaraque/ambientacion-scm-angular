c01869e6c6dd4d668705fe96b39b10e6d1bbc979
Mauricio Andres Pulgarin Araque <mauricio.pulgarin@softtekprojects.com>
2026-03-04 12:31:38 -0500

    cambio ruta apiURL

--- app/frontend/src/app/services/api.service.ts
+++ app/frontend/src/app/services/api.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://127.0.0.1:8001/api';
+  private apiUrl = '/api';
 
   constructor(private http: HttpClient) { }
 
